ac4a99c07dcd3361dd3024c0ff13659c4c6f9bd9
Artem Polikarpov <artemiypolikarpov@gmail.com>
2021-07-15 13:40:56 +0300

    Init AppNavigation component

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,29 @@ import {
 } from "react-router-dom";
 
 import './App.css';
+import AppNavigation from './layout/AppNavigation';
 import SignUpPage from './pages/SignUp';
+
+
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/sign-up">
-          <SignUpPage />
-        </Route>
-        <Route>
-          <Redirect exact from="/" to="/sign-up" />
-        </Route>
-      </Switch>
-    </Router>
+    <>
+      <AppNavigation />
+      <main>
+        <Router>
+          <Switch>
+            <Route path="/sign-up">
+              <SignUpPage />
+            </Route>
+            <Route>
+              <Redirect exact from="/" to="/sign-up" />
+            </Route>
+          </Switch>
+        </Router>
+      </main>
+    </>
+
   );
 }
 
---
+++ src/layout/AppNavigation/index.tsx
@@ -0,0 +1,25 @@
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+export const AppNavigation = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6">
+          React Blog
+        </Typography>
+        <Button color="inherit">Sign In</Button>
+        <Button color="inherit">Sign Up</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default AppNavigation;
