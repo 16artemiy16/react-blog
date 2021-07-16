f4b6f20bf3d3f946d9cdfbcc351bbfdd7ead1176
Artem Polikarpov <artemiypolikarpov@gmail.com>
2021-07-16 12:27:51 +0300

    Implement form in Sign In page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,7 @@
-import { TextField } from '@material-ui/core';
+import { Box, TextField } from '@material-ui/core';
 import styled from 'styled-components';
 import Button from '@material-ui/core/Button';
+import { useState, ChangeEvent } from 'react';
 
 const DivMain = styled.div`
   display: flex;
@@ -22,17 +23,43 @@ const Form = styled.form`
   }
 `;
 
+const DivError = styled.div`
+  color: var(--error);
+`;
+
 export const SignInPage = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [error, setError] = useState<string | null>(null);
+
+  const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value)
+  };
+
+  const handleLogIn = () => {
+    setError('The user with this email does not exist or password is incorrect!');
+  };
+
   return (
     <DivMain>
       <h1>Sign In</h1>
       <Form autoComplete="off">
-        <TextField label="Email" type="text" margin="normal" />
-        <TextField label="Password" type="password" margin="normal" />
+        <TextField label="Email" type="text" margin="normal" value={email} onChange={handleEmailChange} />
+        <TextField label="Password" type="password" margin="normal" value={password} onChange={handlePasswordChange}/>
 
-        <Button variant="contained" color="primary">
+        <Button variant="contained" color="primary" onClick={handleLogIn}>
           Sign In
         </Button>
+
+        { error &&
+          <Box mt={3}>
+            <DivError>{ error }</DivError>
+          </Box>
+        }
       </Form>
     </DivMain>
   );
