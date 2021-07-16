d3c54ca1f1d1ba87148b922c436f4a533097b043
Artem Polikarpov <artemiypolikarpov@gmail.com>
2021-07-16 13:10:58 +0300

    Implement Sign Up page mock up

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,68 @@
+import styled from 'styled-components';
+import { TextField } from '@material-ui/core';
+import { ChangeEvent, useState } from 'react';
+import Button from '@material-ui/core/Button';
+
+const DivMain = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  
+  & .MuiFormControl-root {
+    margin: 1em 0;
+  }
+  
+  & .MuiButton-root {
+    margin-top: 2em;
+  }
+`;
+
+const DivError = styled.div`
+  color: var(--error);
+`;
+
+
 const SignUpPage = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [passwordAgain, setPasswordAgain] = useState<string>('');
+
+  const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handlePasswordAgainChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPasswordAgain(event.target.value);
+  };
+
+  const handleSignUp = () => {
+
+  };
+
   return (
-    <div>Sign Up page</div>
+    <DivMain>
+      <h1>Sign Up</h1>
+
+      <Form autoComplete="off">
+        <TextField label="Email" type="email" margin="normal" value={email} onChange={handleEmailChange} />
+        <TextField label="Password" type="password" margin="normal" value={password} onChange={handlePasswordChange}/>
+        <TextField label="Password again" type="password" margin="normal" value={passwordAgain} onChange={handlePasswordAgainChange}/>
+
+        <Button variant="contained" color="primary" onClick={handleSignUp}>
+          Sign Up
+        </Button>
+      </Form>
+    </DivMain>
   );
 };
 
