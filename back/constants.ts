526bb941a9a144999e13cfbf996b49a35c23baa9
Artem Polikarpov <artemiypolikarpov@gmail.com>
2021-07-25 16:55:07 +0300

    Install mongodb and mongoose

--- back/constants.ts
+++ back/constants.ts
@@ -1,1 +1,2 @@
 export const DEFAULT_PORT = 3001;
+export const DEFAULT_MONGO_URI = 'mongodb://localhost1/react-blog';
--- back/index.ts
+++ back/index.ts
@@ -1,11 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
 import { connectRoutes } from './routes';
-import { DEFAULT_PORT } from './constants';
+import { DEFAULT_MONGO_URI, DEFAULT_PORT } from './constants';
 
 const app = express();
 
 connectRoutes(app);
 
-app.listen(DEFAULT_PORT, () => {
-  console.log('React block BACK is running on PORT ' + DEFAULT_PORT);
-});
+mongoose.connect(DEFAULT_MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    app.listen(DEFAULT_PORT, () => {
+      console.log('React block BACK is running on PORT ' + DEFAULT_PORT);
+    });
+  });
